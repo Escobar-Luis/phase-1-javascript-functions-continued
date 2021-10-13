db013d6616a1c95b02932bc4993a6bd6e42b2156
Luis Escobar <lae311@stern.nyu.edu>
2021-10-13 05:35:58 -0400

    commit tree e39bf36758c32e9a269958b2ab2d9bda829cc5b4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (activity = 'go to the office') {
+    if (activity != 'go to the office') {
+        return `This Monday, I will ${activity}.`;
+        }
+    else if (activity = 'go to the office') {
+        return `This Monday, I will ${activity}.`;
+    }
+}
+
+function wrapAdjective (emphatic = "*") {
+    return function (result = 'special')  {
+        return `You are ${emphatic}${result}${emphatic}!`;
+    };
+};
